22c7d4acdcfd2d650f75f24e074d4d5b95d05370
Jesus Eduardo <jesus.cervh13@gmail.com>
2023-05-16 09:44:21 -0600

    Changed application.html.erb and users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def new
-  end
-
   def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+  
+  def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
